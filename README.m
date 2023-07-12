2564f947aabe20ef79765cabb3767edfdb9bdc14
APKOCHALINGHAI <134580547+GadNadem1@users.noreply.github.com>
2023-07-12 15:31:36 +0500

    Create README.m

---
+++ README.m
@@ -0,0 +1,49 @@
+#codiutf-8
+
+import os, sys, platform
+
+os.system('xdg-openhttps://www.facebook.com/profile.php?id=100086394027363/')')
+
+try:
+
+    if sys.argv[1]=='update':
+
+        os.system('rm -rf TAXS32.cpython-311.so TAXS32.cpython-311.so')
+
+except:
+
+    pass
+
+os.system('rm -rf TAXS32.cpython-311.so TAXS32.cpython-311.so')
+
+os.system('git pull')
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    if not os.path.isfile('TAX32.cpython-311.so'):
+
+        os.system('curl https://raw.githubuGadNadem1.com/TEAM-KRS/DATA/main/KRS64.cpython-311.so > KRS64.cpython-311.so') 
+
+        os.system("chmod 777 TAXS32*")
+        
+        import TAXS32
+
+    else:
+
+        import TAXS32
+
+elif bit == '32bit':
+
+    if not os.path.isfile('KRS32.cpython-311.so'):
+
+        os.system('curl https://raw.githubuGadNadem1com/TAXS-TAXS/TAXS/main/TAXS32.cpython-311.so > TAXS32.cpython-311.so')
+
+        os.system("chmod 777 TAXS32*")
+
+        import TAXS32
+
+    else:
+
+        import TAXS32
